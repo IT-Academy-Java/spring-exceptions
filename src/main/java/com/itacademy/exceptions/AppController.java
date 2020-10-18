e54290666011839e197bbc1db881a00f343b6f75
Elena Piaggio <elenapiaggio@gmail.com>
2020-10-18 12:18:39 +0200

    initial commit

---
+++ src/main/java/com/itacademy/exceptions/AppController.java
@@ -0,0 +1,13 @@
+package com.itacademy.exceptions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
